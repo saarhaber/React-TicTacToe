2d71a3b50a46d87a34633b051e976c0689075eed
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2019-02-14 13:38:23 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      board : Array(9).fill(null),
+      player: "X"
+    }
+  }
+  handleClick(index) {
+    let newBoard = this.state.board
+    if(this.state.board[index]===null) {
+      newBoard[index]= this.state.player
+      this.setState({
+      board: newBoard,
+      player: this.state.player ==="X" ? "O" : "X"
+    })
+    }
+
+  }
   render() {
+    const Box = this.state.board.map(
+      (box, index)=>
+      <div className="box"
+      key={index}
+    onClick={()=>this.handleClick(index)}>
+    {box}
+    </div>)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <h1>Tic Tac Toe App</h1>
+        <div className="board">
+        {Box}
+        </div>
       </div>
     );
   }
