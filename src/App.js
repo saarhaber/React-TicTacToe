2e3425ea7152df4c50808a7e9e8b68f8b6e5a4df
Saar Haber <37675905+saarhaber@users.noreply.github.com>
2019-02-14 19:26:26 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Player from './components/choosePlayer';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      board : Array(9).fill(null),
+      player: "null"
+    }
+  }
+  checkWinner(){
+    let winLines =
+    [
+      ["0","1","2"],
+      ["3","4","5"],
+      ["6","7","8"],
+      ["0","3","6"],
+      ["1","4","7"],
+      ["2","5","8"],
+      ["0","4","8"],
+      ["2","4","6"],
+    ]
+
+for (let index = 0; index < winLines.length; index++) {
+  const [a,b,c] = winLines[index];
+  if (this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c] && this.state.board[a]){
+    alert('You won');
+    this.setState({
+      winner: this.state.player
+    })
+  }
+}
+
+
+  }
+  handleClick(index) {
+    if(this.state.player  && !this.state.winner){
+    let newBoard = this.state.board
+    if(this.state.board[index]===null) {
+      newBoard[index]= this.state.player
+      this.setState({
+      board: newBoard,
+      player: this.state.player ==="X" ? "O" : "X"
+    })
+
+    this.checkWinner()
+    }
+  }
+}
+
+  setPlayer(player){
+      this.setState({ player })
+  }
   render() {
+    const Box = this.state.board.map(
+
+      (box, index)=>
+
+      <div className="box"
+
+      key={index}
+
+    onClick={()=>this.handleClick(index)}>
+
+    {box}
+
+    </div>)
+
+let status  = this.state.player? <h2>Next Player is {this.state.player}</h2> :<Player player={(e)
+  =>this.setPlayer(e)}/>
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      <div className="container">
+
+        <h1>Tic Tac Toe App</h1>
+
+        {status}
+
+        <div className="board">
+
+        {Box}
+
+        </div>
+
       </div>
     );
   }
